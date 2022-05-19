0eac695c6ca30a771480fbdcd864a6ff9699e7f3
yu_sato <howwednes@gmail.com>
2022-05-19 13:08:00 +0900

    add wpf injection(don't work)

--- wpftest/wpftest/App.xaml.cs
+++ wpftest/wpftest/App.xaml.cs
@@ -5,13 +5,34 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Wpftest.ViewModels;
 
-namespace Wpftest
+namespace Wpftest;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private ServiceProvider serviceProvider;
+    public App(): base()
     {
+        ServiceCollection services = new ServiceCollection();
+        ConfigureServices(services);
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    private void ConfigureServices(ServiceCollection services)
+    {
+        services.AddHttpClient<MainWindowViewModel>();
+        services.AddSingleton<MainWindow>();
+    }
+
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        var mainWindow = serviceProvider.GetService<MainWindow>();
+        mainWindow.Show();
     }
 }
+
---
+++ wpftest/wpftest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using efcoretest.Models;
+using Reactive.Bindings;
+
+namespace Wpftest.ViewModels;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    // Viewにバインドされるコマンド
+    public ReactiveCommand LoadedCommand { get; } = new();
+
+    private HttpClient client;
+
+    public List<Customer> Customers = new List<Customer>();
+
+    public MainWindowViewModel(HttpClient client)
+    {
+        this.client = client;
+        LoadedCommand.Subscribe(() =>
+        {
+            // ここにロード時の処理を記載
+            System.Console.WriteLine("Loaded !!");
+        });
+
+        Customers.Add(new Customer{ID=1,Address="hoge",CompanyName = "fuga", Phone ="00-99"});
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
